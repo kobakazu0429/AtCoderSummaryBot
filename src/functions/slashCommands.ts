f0d060b73a9844ee62180d24f60030a2b0f676c7
kobakakzu0429 <kobakazu10@gmail.com>
2020-08-09 09:54:31 +0900

    split file AtCoder class

--- src/functions/slashCommands.ts
+++ src/functions/slashCommands.ts
@@ -1,82 +1,7 @@
-import { APIGatewayProxyEvent } from "aws-lambda";
-import * as cheerio from "cheerio";
+import { APIGatewayProxyHandler, APIGatewayProxyEvent } from "aws-lambda";
 import axios from "axios";
 import * as querystring from "querystring";
-
-interface AtCoderResult {
-  time: Date;
-  title: string;
-  status: string;
-  detail: string;
-}
-
-class AtCoder {
-  constructor(contestId: string, atCoderId: string) {
-    const baseUrl = "https://atcoder.jp";
-    this.baseUrl = baseUrl;
-    const url = `${baseUrl}/contests/${contestId}/submissions?f.Task=&f.Language=&f.Status=&f.User=${atCoderId}`;
-    this.url = url;
-  }
-
-  private baseUrl: string;
-  private url: string;
-
-  async scrapingAtCoderContestResult() {
-    const raw = await this.getRawHtml();
-    const result = this.parse(raw);
-    const summry = this.summry(result);
-    return summry;
-  }
-
-  public get userResultPageUrl(): string {
-    return this.url;
-  }
-
-  async getRawHtml() {
-    const result = await axios.get<string>(this.url);
-    return result.data;
-  }
-
-  parse(html: string) {
-    const $ = cheerio.load(html);
-    const tr = $("tr");
-    const td = tr.slice(1).map((_i, el) => $(el).find("td"));
-    const result: AtCoderResult[] = [];
-    td.each((_i, el) => {
-      const timeEl = $(el).eq(0);
-      const titleEl = $(el).eq(1);
-      const statusEl = $(el).eq(6);
-      const detailEl = $(el).eq(9);
-      const time = timeEl.find("time").text();
-      const title = titleEl.text();
-      const status = statusEl.text();
-      const detail = detailEl.find("a").attr("href");
-
-      result.push({
-        time: new Date(time),
-        title,
-        status,
-        detail: this.baseUrl + detail,
-      });
-    });
-
-    return result;
-  }
-
-  summry(data: AtCoderResult[]) {
-    const summery = {};
-
-    data.forEach((v) => {
-      const time = new Date(v.time);
-      const Q = v.title[0];
-      if (summery[Q] && summery[Q].time > time) return;
-
-      summery[Q] = { ...v, time };
-    });
-
-    return summery;
-  }
-}
+import { AtCoder, AtCoderResult } from "../AtCoder";
 
 function normalizer(text: string) {
   const prettyData = text.split(/“|”|"|'|\s/).filter((v) => v);
@@ -204,7 +129,11 @@ function msgBuilder(contestId: string, data: Data[]) {
   return [header, divider, ...body];
 }
 
-export const handler = async (event: any, context: any, callback: any) => {
+export const handler: APIGatewayProxyHandler = async (
+  event,
+  _context,
+  callback
+) => {
   try {
     // solution of timed out by 3,000 ms
     callback(null, { statusCode: 202, body: "" });
